8fc4412a7d578a1e8925c8b8a37caac719f24c33
sschiglintsev <s.s.chiglintsev@gmail.com>
2022-04-25 16:39:40 +0500

    49

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, {ChangeEvent} from 'react';
 import clsses from './Dialogs.module.css'
 import {Dialog} from "./Dialog/Dialog";
 import {Message} from "./Message/Message";
-import {dialogsType, messagesType} from "../../Redux/State";
+import {Button, TextField} from "@material-ui/core";
+import {messagesPageType} from "../../Redux/reducer-dialogs";
 
 type DialogsPropsType = {
-    propsDialogs:Array<dialogsType>,
-    propsMessages:Array<messagesType>
+    changeMessage:(title:string) => void,
+    addMessage:() => void,
+    propsDialogsMessage: messagesPageType,
 }
 
-export const Dialogs = (props:DialogsPropsType) => {
+export const Dialogs = (props: DialogsPropsType) => {
 
-    const NewDialogs = props.propsDialogs.map(d => <Dialog key={d.id} name={d.name} id={d.id}/>)
-    const NewMessages = props.propsMessages.map(m => <Message key={m.id} message={m.message}/>)
+    const NewDialogs = props.propsDialogsMessage.dialogs.map(d => <Dialog key={d.id} name={d.name} id={d.id}/>)
+    const NewMessages = props.propsDialogsMessage.messages.map(m => <Message key={m.id} message={m.message}/>)
+
+    const onChangeMessageHandler = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        props.changeMessage(e.target.value)
+    }
+    const onClickAddMessageHandler = () => {
+        props.addMessage()
+    }
 
     return (
         <div className={clsses.dialogs}>
@@ -21,6 +30,23 @@ export const Dialogs = (props:DialogsPropsType) => {
             </div>
             <div className={clsses.messages}>
                 {NewMessages}
+                <div>
+
+                    <TextField
+                        value={props.propsDialogsMessage.textMessage}
+                        onChange={onChangeMessageHandler}
+                        id="outlined-basic"
+                        label="Enter you massage"
+                        variant="outlined"
+                        style={{background:'white'}}
+
+                    />
+
+                    <p></p>
+                    <Button variant="contained" onClick={onClickAddMessageHandler} >
+                        Add message
+                    </Button>
+                </div>
             </div>
         </div>
     );
