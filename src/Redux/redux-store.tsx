f7e9e7303e0306b23aa50d3b16813e95d2df54d6
sschiglintsev <s.s.chiglintsev@gmail.com>
2022-06-06 14:33:01 +0500

    thunk

--- src/Redux/redux-store.tsx
+++ src/Redux/redux-store.tsx
@@ -1,14 +1,17 @@
-import {combineReducers, createStore} from "redux";
+import {applyMiddleware, combineReducers, createStore} from "redux";
 import {reducerPosts} from "./reducer-profile";
 import {reducerDialogs} from "./reducer-dialogs";
 import {reducerUsers} from "./reducer-users";
+import {reducerAuth} from "./reducer-auth";
+import thunk from "redux-thunk";
 
 const rootReducer = combineReducers({
     profilePage:reducerPosts,
     messagesPage:reducerDialogs,
-    userPage:reducerUsers
+    userPage:reducerUsers,
+    Auth:reducerAuth
 })
 export type rootReducerType = ReturnType<typeof rootReducer>
 
-export const store = createStore(rootReducer);
+export const store = createStore(rootReducer, applyMiddleware(thunk));
 export  type reduxStoreType = typeof store
